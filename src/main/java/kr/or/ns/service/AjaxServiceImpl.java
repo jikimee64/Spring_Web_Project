aae19078e0c18b1712457977b0281a38b2c02953
jikimee64 <jikimee64@gmail.com>
2020-07-01 20:44:00 +0900

    [슬기 : 아이디 인증시 메일 전송]

--- src/main/java/kr/or/ns/service/AjaxServiceImpl.java
+++ src/main/java/kr/or/ns/service/AjaxServiceImpl.java
@@ -1,5 +1,62 @@
 package kr.or.ns.service;
 
+import java.sql.SQLException;
+import java.util.HashMap;
+
+import javax.inject.Inject;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailSender;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
+
+import kr.or.ns.dao.AjaxRestDao;
+import kr.or.ns.util.MailHandler;
+import kr.or.ns.util.Tempkey;
+
+@Service
 public class AjaxServiceImpl implements AjaxService {
+	@Autowired
+	private SqlSession sqlsession;
+
+	@Inject
+	private JavaMailSender mailSender;
+
+	// 이름과 이메일 받아서 존재하는 회원인지 확인
+	public int emailCheck(String user_name, String user_email) {
+		AjaxRestDao dao = sqlsession.getMapper(AjaxRestDao.class);
+		int result = 0;
+		HashMap<String, String> map = new HashMap<String, String>();
+		map.put("user_name", user_name);
+		map.put("user_email", user_email);
+		result = dao.emailCheck(map);
+		return result;
+	}
+
+	// 이메일 전송하는 서비스
+	public String emailSend(String user_email) {
+		String key = new Tempkey().getKey(6, false);
+
+		try {
+
+			MailHandler sendMail = new MailHandler(mailSender);
+
+			// 제목
+			sendMail.setSubject("[이메일 인증번호]");
+			// 내용
+			sendMail.setText(new StringBuffer().append("<h1>메일인증번호입니다</h1>").append("발품에 가입해주셔서 감사합니다. <br/>")
+					.append("고객님의 인증번호는 [ " + key + " ] 입니다.").toString());
+			// 보낸사람
+			sendMail.setFrom("balpoom@balpoom.com", ".노상");
+			// 받는사람
+			sendMail.setTo(user_email);
+			sendMail.send();
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+		return null;
+	}
 
 }
--- src/main/java/kr/or/ns/vo/Users.java
+++ src/main/java/kr/or/ns/vo/Users.java
@@ -5,5 +5,77 @@ import lombok.Setter;
 import lombok.ToString;
 
 public class Users {
-
+	private String USER_ID;
+	private String USER_PWD;
+	private int ENABLED;
+	private String USER_NAME;
+	private String USER_EMAIL;
+	private String PROFILE_IMG;
+	private String NICKNAME;
+	private String INTRODUCE;
+	private int BLAME_COUNT;
+	
+	
+	public String getUSER_ID() {
+		return USER_ID;
+	}
+	public void setUSER_ID(String uSER_ID) {
+		USER_ID = uSER_ID;
+	}
+	public String getUSER_PWD() {
+		return USER_PWD;
+	}
+	public void setUSER_PWD(String uSER_PWD) {
+		USER_PWD = uSER_PWD;
+	}
+	public int getENABLED() {
+		return ENABLED;
+	}
+	public void setENABLED(int eNABLED) {
+		ENABLED = eNABLED;
+	}
+	public String getUSER_NAME() {
+		return USER_NAME;
+	}
+	public void setUSER_NAME(String uSER_NAME) {
+		USER_NAME = uSER_NAME;
+	}
+	public String getUSER_EMAIL() {
+		return USER_EMAIL;
+	}
+	public void setUSER_EMAIL(String uSER_EMAIL) {
+		USER_EMAIL = uSER_EMAIL;
+	}
+	public String getPROFILE_IMG() {
+		return PROFILE_IMG;
+	}
+	public void setPROFILE_IMG(String pROFILE_IMG) {
+		PROFILE_IMG = pROFILE_IMG;
+	}
+	public String getNICKNAME() {
+		return NICKNAME;
+	}
+	public void setNICKNAME(String nICKNAME) {
+		NICKNAME = nICKNAME;
+	}
+	public String getINTRODUCE() {
+		return INTRODUCE;
+	}
+	public void setINTRODUCE(String iNTRODUCE) {
+		INTRODUCE = iNTRODUCE;
+	}
+	public int getBLAME_COUNT() {
+		return BLAME_COUNT;
+	}
+	public void setBLAME_COUNT(int bLAME_COUNT) {
+		BLAME_COUNT = bLAME_COUNT;
+	}
+	@Override
+	public String toString() {
+		return "Users [USER_ID=" + USER_ID + ", USER_PWD=" + USER_PWD + ", ENABLED=" + ENABLED + ", USER_NAME="
+				+ USER_NAME + ", USER_EMAIL=" + USER_EMAIL + ", PROFILE_IMG=" + PROFILE_IMG + ", NICKNAME=" + NICKNAME
+				+ ", INTRODUCE=" + INTRODUCE + ", BLAME_COUNT=" + BLAME_COUNT + "]";
+	}
+	
+	
 }
