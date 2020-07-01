4622311639b26ad8d63625bf752635821bd6eb9f
jikimee64 <jikimee64@gmail.com>
2020-07-01 14:29:10 +0900

    [우철 : fronted폴더 삭제]

--- src/main/java/kr/or/ns/controller/MyPageController.java
+++ src/main/java/kr/or/ns/controller/MyPageController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/mypage/")
-public class MypageController {
+public class MyPageController {
 	
 	@RequestMapping("/mypage.do")
 	public String myPagePage() {
