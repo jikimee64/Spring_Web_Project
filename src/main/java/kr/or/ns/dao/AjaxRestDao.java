6422f5737ee1d5fbae6bb1d2c17191e833ee67fe
ksks723 <ksks723@gmail.com>
2020-07-02 20:07:51 +0900

    슬기 : 아이디찾기 - (인증번호발급), 비밀번호찾기 - (임시비밀번호발급) ,  velocity 사용

--- src/main/java/kr/or/ns/dao/AjaxRestDao.java
+++ src/main/java/kr/or/ns/dao/AjaxRestDao.java
@@ -1,9 +1,12 @@
 package kr.or.ns.dao;
 
-import java.sql.SQLException;
 import java.util.HashMap;
+
+import kr.or.ns.vo.Users;
 
 public interface AjaxRestDao {
 	//이름과 이메일 받아서 존재하는 회원인지 확인
 	public int emailCheck(HashMap<String, String> map) ;
+	public Users searchId(HashMap<String, String> map);
+	public void updatePw(Users vo);
 }
--- src/main/java/kr/or/ns/service/AjaxService.java
+++ src/main/java/kr/or/ns/service/AjaxService.java
@@ -1,10 +1,13 @@
 package kr.or.ns.service;
 
 import org.apache.ibatis.session.SqlSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 public interface AjaxService {
 	public int emailCheck(String user_name, String user_email);
 	public String emailSend(String useremail);
+	public String findId(String user_name, String user_email);
+	public void makeNewPw(String userid, String useremail);
 }
