f4c2fbf5b30751c68f4e6abf0d1565467e4b68ef
yeonkyujeon <ehb6915@naver.com>
2020-07-03 19:27:48 +0900

    연규 : 스터디강의 리스트 5개씩 나눠서 뿌려주기 연습해볼려고 미네누나한테 푸쉬할려고 함

--- src/main/java/kr/or/ns/controller/LectureController.java
+++ src/main/java/kr/or/ns/controller/LectureController.java
@@ -1,10 +1,16 @@
 package kr.or.ns.controller;
 
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import kr.or.ns.service.BoardServiceImpl;
+import kr.or.ns.page.PageMaker;
+import kr.or.ns.service.LectureServiceImpl;
+import kr.or.ns.vo.Criteria;
 
 @Controller
 @RequestMapping("/board/")
@@ -12,18 +18,36 @@ public class LectureController {
 	
 	
 	@Autowired
-	private BoardServiceImpl service;
+	private LectureServiceImpl service;
 	
 	
-
+	//스터디목록 + 페이징
 	@RequestMapping("course_List.do")
-	public String courseListPage() {
+	public String courseListPage(Criteria cri,Model model) {
 		System.out.println("강좌페이지로 이동이동(연규가씀)");
-
-		return "/user/board/course_List";
+		System.out.println(cri.getPage());
+		PageMaker pageMaker = new PageMaker();
+		pageMaker.setCri(cri);
+		pageMaker.setTotalCount(service.getLectureCount());
+		
+		//DAO받아오기 + 매퍼를 통한 인터페이스 연결
+		System.out.println(cri.getPage());
+		System.out.println(cri.getPageStart());
+		System.out.println(cri.getPerPageNum());
+		List<Map<String,Object>> list = null;
+		list = service.getLectureList(cri);
+		model.addAttribute("list",list); //view까지 전달(forward)
+		model.addAttribute("pageMaker",pageMaker); 
+		
+		
+		return "user/board/course_List"; //study_List.html
 	}
 	
-
+	
+	
+	
+	
+	
 //	@RequestMapping("course_List.do")
 //	public String courseListPage(Model m odel) {
 //		System.out.println("강좌페이지로 이동이동(연규가씀)");
