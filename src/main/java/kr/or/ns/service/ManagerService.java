1fbab7cbd3b1606724cb9ec62439536f64aeca56
yeonkyujeon <ehb6915@naver.com>
2020-07-09 16:29:52 +0900

    [연규 : 관리자 회원리스트 뿌리기 + 상세보기]

--- src/main/java/kr/or/ns/service/ManagerService.java
+++ src/main/java/kr/or/ns/service/ManagerService.java
@@ -1,9 +1,15 @@
 package kr.or.ns.service;
 
+import java.util.HashMap;
 import java.util.List;
 
 import kr.or.ns.vo.Users;
 
 public interface ManagerService {
 	public List<Users> getMemberList();
+	
+	// 
+	public Users getUsers(String user_id);
+	
+	public List<HashMap<String, String>> getSkill(String user_id);
 }
--- src/main/java/kr/or/ns/service/ManagerServiceImpl.java
+++ src/main/java/kr/or/ns/service/ManagerServiceImpl.java
@@ -1,13 +1,16 @@
 package kr.or.ns.service;
 
+import java.util.HashMap;
 import java.util.List;
 
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import kr.or.ns.dao.ManagerDao;
+import kr.or.ns.dao.MyPageDao;
 import kr.or.ns.vo.Users;
-
+@Service
 public class ManagerServiceImpl implements ManagerService {
 	
 	@Autowired
@@ -22,6 +25,23 @@ public class ManagerServiceImpl implements ManagerService {
 		
 		
 	return list;
+	}
+
+
+	@Override
+	public Users getUsers(String user_id) {
+		ManagerDao dao = sqlsession.getMapper(ManagerDao.class);
+		Users user = dao.getUsers(user_id);
+		return user;
+	}
+
+
+	@Override
+	public List<HashMap<String, String>> getSkill(String user_id) {
+		ManagerDao dao = sqlsession.getMapper(ManagerDao.class);
+		List<HashMap<String, String>> list = dao.getSkill(user_id);
+		System.out.println("욘두 : " + list);
+		return list;
 	}
 
 }
