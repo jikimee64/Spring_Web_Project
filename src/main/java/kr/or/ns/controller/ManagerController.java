9059bfe11fa2be3fdcbe04303747b7122b912385
yeonkyujeon <ehb6915@naver.com>
2020-07-09 09:46:48 +0900

    [연규 : 페이지0버튼 없애기]

--- src/main/java/kr/or/ns/controller/ManagerController.java
+++ src/main/java/kr/or/ns/controller/ManagerController.java
@@ -1,11 +1,22 @@
 package kr.or.ns.controller;
 
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import kr.or.ns.page.PageMaker;
+import kr.or.ns.vo.Criteria;
 
 @Controller
 @RequestMapping("/manager/")
 public class ManagerController {
+
+	
+	
+	
 	
 	@RequestMapping("index.do")
 	public String indexPage() {
@@ -22,8 +33,27 @@ public class ManagerController {
 	}
 
 	@RequestMapping("board/member_Management.do")
-	public String memberManagementPage() {
+	public String memberManagementPage(Criteria cri, Model model) {
 		System.out.println("어드민 회원관리 테이블페이지이동");
+		
+//		System.out.println(cri.getPage());
+//		PageMaker pageMaker = new PageMaker();
+//		pageMaker.setCri(cri);
+//		pageMaker.setTotalCount(service.getMemberCount());
+//		
+//		//DAO받아오기 + 매퍼를 통한 인터페이스 연결
+//		System.out.println(cri.getPage());
+//		System.out.println(cri.getPageStart());
+//		System.out.println(cri.getPerPageNum());
+//		List<Map<String,Object>> list = null;
+//		list = service.getMemberList(cri);
+//		model.addAttribute("list", list); //view까지 전달(forward)
+//		model.addAttribute("pageMaker",pageMaker); 
+//		
+//		System.out.println("미네미네미네미네미네미넴니ㅔ " + pageMaker.isNext());
+//		System.out.println(list.toString());
+//		System.out.println("컨트롤러2");
+		
 
 		return "manager/board/member_Management"; 
 	}
