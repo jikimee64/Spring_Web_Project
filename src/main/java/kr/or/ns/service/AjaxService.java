89d09d79360f39d54d3ac0e7dd07629121c22735
jjunga <junga710@naver.com>
2020-07-10 09:33:12 +0900

    [정아 : 오전커밋, 신고기능 진행중]

--- src/main/java/kr/or/ns/service/AjaxService.java
+++ src/main/java/kr/or/ns/service/AjaxService.java
@@ -7,14 +7,25 @@ import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-public interface AjaxService {
-	public int emailCheck(String user_name, String user_email);
-	public String emailSend(String useremail);
-	public String findId(String user_name, String user_email);
-	public void makeNewPw(String userid, String useremail);
+import kr.or.ns.vo.Blame;
 
-	public int searchId(String user_id, String user_email);
-	public int idcheck(String user_id) throws ClassNotFoundException;
+public interface AjaxService {
 	
+	
+	public int emailCheck(String user_name, String user_email);
+
+	public String emailSend(String useremail);
+
+	
+	public String findId(String user_name, String user_email);
+
+	public void makeNewPw(String userid, String useremail);
+	
+	public int searchId(String user_id, String user_email);
+	//아이디 중복체크
+	public int idcheck(String user_id) throws ClassNotFoundException;
+	//스터디 게시판 일반 지원하기
 	public int applyNomalStudy(String user_id, String s_seq);
+	
+	public int blameInsert(Blame blame,  );
 }
