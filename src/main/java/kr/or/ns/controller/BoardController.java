fbf1846d299b0b8a226d41ae9ff642ecd317cea5
jjunga <junga710@naver.com>
2020-07-10 10:51:55 +0900

    [정아 BoardController.java 수정]

--- src/main/java/kr/or/ns/controller/BoardController.java
+++ src/main/java/kr/or/ns/controller/BoardController.java
@@ -1,11 +1,10 @@
 package kr.or.ns.controller;
 
-import java.io.FileOutputStream;
-import java.io.IOException;
+
+
 import java.security.Principal;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.HashMap;
+
 import java.util.List;
 import java.util.Map;
 
@@ -14,20 +13,18 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestBody;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.multipart.commons.CommonsMultipartFile;
 
-import kr.or.ns.page.PageMaker;
+
+
 import kr.or.ns.page.PageMaker_Board;
 import kr.or.ns.service.BoardServiceImpl;
-import kr.or.ns.vo.Criteria;
+
 import kr.or.ns.vo.Criteria_Board;
 import kr.or.ns.vo.Study;
-import kr.or.ns.vo.Users;
+
 
 /*
 클래스명 : BoardController
