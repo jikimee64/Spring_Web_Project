d99b665adb69f3b55f9596206f3adf39b15b611a
yeonkyujeon <ehb6915@naver.com>
2020-07-14 11:50:12 +0900

    [연규 : 크롤링 컨트롤러 조금 작업하다가 말음]

--- src/main/java/kr/or/ns/controller/CrawlingController.java
+++ src/main/java/kr/or/ns/controller/CrawlingController.java
@@ -2,15 +2,29 @@ package kr.or.ns.controller;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.HttpServerErrorException;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @RestController
 @RequestMapping("/Crawling/")
@@ -126,5 +140,51 @@ public class CrawlingController {
 		System.out.println("크롤링 페이지 이동");
 		return titleList;
 	}
+	
 
+	@RequestMapping("CrawlingUdemy1.do")
+	public String Udemy() {
+
+        HashMap<String, Object> result = new HashMap<String, Object>();
+ 
+        String jsonInString = "";
+ 
+        try {
+ 
+            HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory();
+            factory.setConnectTimeout(5000); //타임아웃 설정 5초
+            factory.setReadTimeout(5000);//타임아웃 설정 5초
+            RestTemplate restTemplate = new RestTemplate(factory);
+ 
+            HttpHeaders header = new HttpHeaders();
+            HttpEntity<?> entity = new HttpEntity<>(header);
+ 
+            String url = "https://www.udemy.com/api-2.0/discovery-units/all_courses/";
+ 
+            UriComponents uri = UriComponentsBuilder.fromHttpUrl(url+"?"+"page_size=16&subcategory=&instructional_level=&lang=&price=&duration=&closed_captions=&subcategory_id=8&source_page=subcategory_page&locale=ko_KR&currency=krw&navigation_locale=en_US&skip_price=true&sos=ps&fl=scat").build();
+ 
+            //이 한줄의 코드로 API를 호출해 MAP타입으로 전달 받는다.
+            ResponseEntity<Map> resultMap = restTemplate.exchange(uri.toString(), HttpMethod.GET, entity, Map.class);
+            result.put("statusCode", resultMap.getStatusCode()); //http status code를 확인
+            result.put("header", resultMap.getHeaders()); //헤더 정보 확인
+            result.put("body", resultMap.getBody()); //실제 데이터 정보 확인
+ 
+            //데이터를 제대로 전달 받았는지 확인 string형태로 파싱해줌
+            ObjectMapper mapper = new ObjectMapper();
+            jsonInString = mapper.writeValueAsString(resultMap.getBody());
+ 
+        } catch (HttpClientErrorException | HttpServerErrorException e) {
+            result.put("statusCode", e.getStatusCode());
+            result.put("body"  , e.getStatusText());
+            System.out.println("dfdfdfdf");
+            System.out.println(e.toString());
+ 
+        } catch (Exception e) {
+            result.put("statusCode", "999");
+            result.put("body"  , "excpetion오류");
+            System.out.println(e.toString());
+        }
+ 
+        return jsonInString;
+	}
 }
