49b69001ee5f87a99d93c0d5d0575b195febd8d8
jikimee64 <jikimee64@gmail.com>
2020-07-15 23:20:51 +0900

    [우철 : 스터디 게시판 다이다믹 쿼리 이용하여 조건에 따른 데이터 추출 성공 / 뷰단에 뿌려주기 남음]

--- src/main/java/kr/or/ns/service/AjaxServiceImpl.java
+++ src/main/java/kr/or/ns/service/AjaxServiceImpl.java
@@ -1,6 +1,7 @@
 package kr.or.ns.service;
 
 import java.security.Principal;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
@@ -327,6 +328,7 @@ public class AjaxServiceImpl implements AjaxService {
 	
 	//스터디 게시판 필터
 	public List<HashMap<String, Object>> studyBoardFilter(HashMap<String, Object> params){
+		
 		AjaxRestDao dao = sqlsession.getMapper(AjaxRestDao.class);
 		System.out.println("유저정보:" + params);
 		List<HashMap<String, Object>> result = dao.studyBoardFilter(params);
