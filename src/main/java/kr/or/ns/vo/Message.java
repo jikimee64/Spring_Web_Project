046b42d096f69b3222d597ab9a7b0590bc98c69b
Minep <pmh@ge.kornu.ac.kr>
2020-07-16 16:58:41 +0900

    [민혜: 풀용커밋]

--- src/main/java/kr/or/ns/vo/Message.java
+++ src/main/java/kr/or/ns/vo/Message.java
@@ -16,54 +16,8 @@ public class Message {
 	private String senderid;
 	private String content;
 	private Date opendate;
-//	private String opendate;
-	
-//	private Date senddate;
-	private String senddate;
-	
-//	
-//	public int getM_seq() {
-//		return m_seq;
-//	}
-//	public void setM_seq(int m_seq) {
-//		this.m_seq = m_seq;
-//	}
-//	public String getReceptionid() {
-//		return receptionid;
-//	}
-//	public void setReceptionid(String receptionid) {
-//		this.receptionid = receptionid;
-//	}
-//	public String getSenderid() {
-//		return senderid;
-//	}
-//	public void setSenderid(String senderid) {
-//		this.senderid = senderid;
-//	}
-//	public String getContent() {
-//		return content;
-//	}
-//	public void setContent(String content) {
-//		this.content = content;
-//	}
-//	public Date getOpendate() {
-//		return opendate;
-//	}
-//	public void setOpendate(Date opendate) {
-//		this.opendate = opendate;
-//	}
-//	
-//	
-//	
-//	
-//	
-//	
-//	public String getSenddate() {
-//		System.out.println("get-vo입니다: "+senddate);
-//		return senddate;
-//	}
-//	
-//	
+	private Date senddate;
+
 //	public void setSenddate(String senddate) {
 //		System.out.println("set-vo입니다: "+senddate);
 //
