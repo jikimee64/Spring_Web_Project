bf0ee8c92bf623b0174416177bdf79ae577ed379
jikimee64 <jikimee64@gmail.com>
2020-07-17 09:52:19 +0900

    [우철 : AOP 코딩중]

--- src/main/java/kr/or/ns/aop/LogTimeCheck.java
+++ src/main/java/kr/or/ns/aop/LogTimeCheck.java
@@ -1,20 +1,24 @@
 package kr.or.ns.aop;
 
+import java.security.Principal;
+
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StopWatch;
 
-import lombok.extern.slf4j.Slf4j;
 
 @Component
 @Aspect
-@Slf4j
 public class LogTimeCheck {
+	
+	static final Logger log = LoggerFactory.getLogger(LogTimeCheck.class);
 
 	@Around("execution(* kr.or.ns.controller.*Controller.*(..))")
 	public Object around(ProceedingJoinPoint joinPoint) throws Throwable {
@@ -25,32 +29,29 @@ public class LogTimeCheck {
 		stopWatch.start();
 		returnObj = joinPoint.proceed();
 		stopWatch.stop();
-		System.out.println("#  " + method + "() Method Performance time : " + stopWatch.getTotalTimeMillis() + "(ms)");
-
+		
 		/*
-		 * OutputStream output = new FileOutputStream("C:/kwc/Output.txt"); String str =
-		 * "오늘 날씨는 아주 좋습니다."; byte[] by = str.getBytes(); output.write(by);
+		 * if(principal != null) { log.warn("# User" + principal.getName() +"| " +
+		 * method + "() Method Performance time : " + stopWatch.getTotalTimeMillis() +
+		 * "(ms)"); }else {
 		 */
-
-		/*
-		 * } catch (Exception e) { e.getStackTrace(); }
-		 */
+			log.warn("# " + method + "() Method Performance time : " + stopWatch.getTotalTimeMillis() + "(ms)");
+		//}
+		
 
 		return returnObj;
 
 	}
+
 	/*
 	 * @Before("execution(* kr.or.ns.controller.*Controller.*(..))") public void
-	 * beforeLog(JoinPoint jp) { System.out.println("@Before"); log.info("[" +
-	 * jp.getTarget().getClass().getSimpleName() + " " + jp.getSignature().getName()
-	 * + " START]"); }
+	 * beforeLog(JoinPoint jp) { System.out.println("@Before"); log.warn("[" +
+	 * jp.getSignature().getName() + " START]"); }
+	 * 
 	 * 
 	 * @After("execution(* kr.or.ns.controller.*Controller.*(..))") public void
-	 * afterLog(JoinPoint jp) { System.out.println("@After"); log.info("[" +
-	 * jp.getTarget().getClass().getSimpleName() + " " + jp.getSignature().getName()
-	 * + " END]");
-	 * 
-	 * }
+	 * afterLog(JoinPoint jp) { System.out.println("@After"); log.warn("[" +
+	 * jp.getSignature().getName() + " END]"); }
 	 */
 
 }
