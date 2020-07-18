4b7fa2d44e4382610d42607e24a284f5c04be392
jikimee64 <jikimee64@gmail.com>
2020-07-18 22:11:33 +0900

    [우철 : 크롤링 컨트롤러 + 서비스 + DB삽입 완료]

---
+++ src/main/java/kr/or/ns/dao/CrawlingDao.java
@@ -0,0 +1,9 @@
+package kr.or.ns.dao;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public interface CrawlingDao {
+	public int insertStudy(HashMap<String, Object> mo);
+}
--- src/main/java/kr/or/ns/vo/Lecture.java
+++ src/main/java/kr/or/ns/vo/Lecture.java
@@ -9,12 +9,18 @@ import lombok.ToString;
 @ToString
 public class Lecture {
 	private int l_seq;
+	private String l_key;
 	private String cate_level;
 	private int p_seq;
 	private int site_seq;
 	private int lan_seq;
-	private String l_title;
-	private String l_content;
 	private String l_image;
+	private String l_title;
+	private String l_writer;
+	private String l_star;
+	private String l_review;
+	private String l_address;
+	
+	
 
 }
